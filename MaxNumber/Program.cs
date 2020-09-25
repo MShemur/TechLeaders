5345b043f28d56bf8c3b541e6d5c7a64f00323af
Максим Шемургалкин <m.shemur@gmail.com>
2020-09-25 19:42:09 +0300

    Add project files.

---
+++ MaxNumber/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new[] { 1, 5, 65, 7, 5, 9, 12, 18, 84, 11, 55, 685, 17, 25, 93, 412, 618, 84 };
+            var count = 4;
+            var counter = 0;
+
+            for (int j = 0; j < count; j++)
+            {
+                var maxIndex = j;
+                var max = array[maxIndex];
+                for (int i = j; i < array.Length; i++)
+                {
+                    if (array[i] > max)
+                    {
+                        max = array[i];
+                        maxIndex = i;
+                    }
+                    counter++;
+                }
+
+                var temp = array[j];
+                array[j] = max;
+                array[maxIndex] = temp;
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(counter);
+            Console.ReadKey();
+        }
+    }
+}
